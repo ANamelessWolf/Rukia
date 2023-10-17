a465a8d3de3f925ea565da0512d266f9daf3c357
Miguel Alanis <anamelessdeath@gmail.com>
2023-10-17 12:10:17 -0600

    Number utilities created

---
+++ Rukia/Utils/NumberUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameless.Libraries.Rukia.ProjectEuler.Utils
+{
+    public static class NumberUtils
+    {
+        /// <summary>
+        /// Test if a number is even
+        /// </summary>
+        /// <param name="number">The number to be tested</param>
+        /// <returns>True if the number is even</returns>
+        public static Boolean IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+
+        /// <summary>
+        /// Test if a number is Odd
+        /// </summary>
+        /// <param name="number">The number to be tested</param>
+        /// <returns>True if the number is Odd</returns>
+        public static Boolean IsOdd(this int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
