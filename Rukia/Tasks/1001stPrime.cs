e4d59b05e23e92e6e9d37142d8771daf555dd0a7
Miguel Alanis <anamelessdeath@gmail.com>
2023-10-22 20:15:18 -0600

    Add problem description

--- Rukia/Tasks/1001stPrime.cs
+++ Rukia/Tasks/1001stPrime.cs
@@ -8,6 +8,10 @@ using System.Threading.Tasks;
 
 namespace Nameless.Libraries.Rukia.ProjectEuler.Tasks
 {
+    /// <summary>
+    /// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+    /// What is the 10001st prime number?
+    /// </summary>
     public class ThousandFirstPrime : ISolution
     {
         public int Number { get;  }
