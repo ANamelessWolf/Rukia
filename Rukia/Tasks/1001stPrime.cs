21ce8a70201e88779d2f125b079476afe019ebd2
Miguel Alanis <anamelessdeath@gmail.com>
2023-10-22 18:49:36 -0600

    10,001 st prime number task

---
+++ Rukia/Tasks/1001stPrime.cs
@@ -0,0 +1,45 @@
+﻿using Nameless.Libraries.Rukia.ProjectEuler.Helper.Interface;
+using Nameless.Libraries.Rukia.ProjectEuler.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameless.Libraries.Rukia.ProjectEuler.Tasks
+{
+    public class ThousandFirstPrime : ISolution
+    {
+        public int Number { get;  }
+        public int Result { get; set; }
+
+        public ThousandFirstPrime(int nst)
+        {
+            this.Number = nst;
+            this.Result = Solve();
+        }
+
+        public string PrintResult()
+        {
+            return String.Format("The {0}st prime number is {1}", this.Number, this.Result);
+        }
+
+        public int Solve()
+        {
+            if (this.Number == 1)
+                return 2;
+
+            int index=2, num = 3, lastPrime=3;
+            PrimeTester pTester = new PrimeTester();
+            while (index < this.Number) { 
+                num+=2;
+                if (pTester.IsPrime(num))
+                {
+                    index++;
+                    lastPrime = num;
+                }
+            }
+            return lastPrime;
+        }
+    }
+}
---
+++ Rukia[test]/ProjectEuler/1001stPrimeTest.cs
@@ -0,0 +1,34 @@
+﻿using Nameless.Libraries.Rukia.ProjectEuler.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameless.Libraries.Rukia.Test.ProjectEuler
+{
+    [TestClass]
+    public class ThousandFirstPrimeTest
+    {
+        [TestMethod]
+        public void Test6ThPrime()
+        {
+            ThousandFirstPrime action = new ThousandFirstPrime(6);
+            Assert.AreEqual(13, action.Result);
+        }
+
+        [TestMethod]
+        public void Test1001StPrime()
+        {
+            ThousandFirstPrime action = new ThousandFirstPrime(1001);
+            Assert.AreEqual(7927, action.Result);
+        }
+
+        [TestMethod]
+        public void Test10001StPrime()
+        {
+            ThousandFirstPrime action = new ThousandFirstPrime(10001);
+            Assert.AreEqual(104743, action.Result);
+        }
+    }
+}
