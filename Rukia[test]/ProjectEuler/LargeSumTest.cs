00c6464e27e82f90e795c3e60dc920b2bc5f34cd
Miguel Alanis <anamelessdeath@gmail.com>
2023-10-27 11:58:01 -0600

    LargeSum solution

---
+++ Rukia[test]/ProjectEuler/LargeSumTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameless.Libraries.Rukia.Test.ProjectEuler
+{
+    [TestClass]
+    public class LargeSumTest
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            LargeSum sum = new LargeSum();
+            Assert.AreEqual("5537376230", sum.Result);
+        }
+    }
+}
