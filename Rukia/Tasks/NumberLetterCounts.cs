fa1e3452a062d11ed1bd5628f42ad0c159c7079a
Miguel Alanis <anamelessdeath@gmail.com>
2023-11-02 22:52:31 -0600

    Number Letters Counts solution

---
+++ Rukia/Tasks/NumberLetterCounts.cs
@@ -0,0 +1,37 @@
+﻿using Nameless.Libraries.Rukia.ProjectEuler.Helper.Interface;
+using Nameless.Libraries.Rukia.ProjectEuler.Utils;
+
+namespace Nameless.Libraries.Rukia.ProjectEuler.Tasks
+{
+    public class NumberLetterCounts : ISolution
+    {
+        public int Limit { get; }
+        public int Count { get; }
+
+
+        public NumberLetterCounts(int limit)
+        {
+            this.Limit = limit;
+            this.Count = this.Solve();
+        }
+
+        public string PrintResult()
+        {
+            return String.Format("The numbers from 1 to {0} inclusive written in words, had a total of {1}", this.Count, this.Limit);
+        }
+
+        public int Solve()
+        {
+            int count = 0;
+            for (int i = 1; i <= this.Limit; i++)
+                count += this.CountLetters(i);
+            return count;
+        }
+
+        public int CountLetters(int number)
+        {
+            var engNum = number.ToEnglish();
+            return engNum.Replace(" ", "").Replace("-", "").Length;
+        }
+    }
+}
---
+++ Rukia[test]/ProjectEuler/NumberLetterCountsTest.cs
@@ -0,0 +1,35 @@
+﻿using Nameless.Libraries.Rukia.ProjectEuler.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameless.Libraries.Rukia.Test.ProjectEuler
+{
+    [TestClass]
+    public class NumberLetterCountsTest
+    {
+        [TestMethod]
+        public void CountTo5()
+        {
+            NumberLetterCounts test = new NumberLetterCounts(5);
+            Assert.AreEqual(19, test.Count);
+        }
+
+        [TestMethod]
+        public void CountTo1000()
+        {
+            NumberLetterCounts test = new NumberLetterCounts(1000);
+            Assert.AreEqual(21124, test.Count);
+        }
+
+        [TestMethod]
+        public void CountNumberChars()
+        {
+            NumberLetterCounts test = new NumberLetterCounts(1);
+            Assert.AreEqual(23, test.CountLetters(342));
+            Assert.AreEqual(20, test.CountLetters(115));
+        }
+    }
+}
