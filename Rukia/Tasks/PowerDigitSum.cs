5e63756128329f3a25e8519e9e473f8ecac2da66
Miguel Alanis <anamelessdeath@gmail.com>
2023-11-02 19:46:07 -0600

    Power Digit sum

---
+++ Rukia/Tasks/PowerDigitSum.cs
@@ -0,0 +1,41 @@
+﻿using Nameless.Libraries.Rukia.ProjectEuler.Helper.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameless.Libraries.Rukia.ProjectEuler.Tasks
+{
+    /// <summary>
+    /// Power digit sum
+    /// 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+    /// What is the sum of the digits of the number 2^1000?
+    /// </summary>
+    public class PowerDigitSum : ISolution
+    {
+        public int Number;
+        public int Exp;
+        public int Result { get; }
+
+        public PowerDigitSum(int number, int exponent)
+        {
+            this.Number = number;
+            this.Exp = exponent;
+            this.Result = this.Solve();
+        }
+
+        public string PrintResult()
+        {
+            return String.Format("The sum of the digits of the number {0}^{1} is {2}", this.Number, this.Exp, this.Result);
+        }
+
+        public int Solve()
+        {
+            BigInteger num = new BigInteger(this.Number);
+            var res = BigInteger.Pow(this.Number, this.Exp);
+            return res.ToString().ToCharArray().Select(x => int.Parse("" + x)).Sum();
+        }
+    }
+}
---
+++ Rukia[test]/ProjectEuler/PowerDigitSumTest.cs
@@ -0,0 +1,27 @@
+﻿using Nameless.Libraries.Rukia.ProjectEuler.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameless.Libraries.Rukia.Test.ProjectEuler
+{
+    [TestClass]
+    public class PowerDigitSumTest
+    {
+        [TestMethod]
+        public void TestPow2_15()
+        {
+            PowerDigitSum test = new PowerDigitSum(2, 15);
+            Assert.AreEqual(26, test.Result);
+        }
+
+        [TestMethod]
+        public void TestPow2_1000()
+        {
+            PowerDigitSum test = new PowerDigitSum(2, 1000);
+            Assert.AreEqual(1366, test.Result);
+        }
+    }
+}
