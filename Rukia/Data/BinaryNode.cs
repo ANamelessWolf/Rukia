ef38f426dcc1ba041654f4c3932bd1a4faa59bb4
Miguel Alanis <anamelessdeath@gmail.com>
2023-11-03 01:00:12 -0600

    Binary tree node

---
+++ Rukia/Data/BinaryNode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameless.Libraries.Rukia.ProjectEuler.Data
+{
+    public class BinaryNode<T>
+    {
+        public T Value { get; set; }
+        public BinaryNode(T value)
+        {
+            this.Value = value;
+        }
+        public BinaryNode<T>[] Parent { get; set; }
+        public BinaryNode<T> Left { get; set; }
+        public BinaryNode<T> Right { get; set; }
+
+        public bool IsRoot { get => this.Parent == null; }
+        public bool IsLeaf { get => this.Left == null && this.Right == null; }
+
+        public void AddParent(BinaryNode<T> node)
+        {
+            if (this.Parent == null)
+                this.Parent = new BinaryNode<T>[] { node };
+            else
+                this.Parent = new BinaryNode<T>[] { this.Parent[0], node };
+        }
+
+        public override string ToString()
+        {
+            string value = "", lft = "", rgt = "";
+            if (this.Value != null)
+                value = "N: " + this.Value.ToString();
+            if (this.Left != null && this.Left.Value != null)
+                lft = "L: " + this.Left.Value.ToString();
+            if (this.Right != null && this.Right.Value != null)
+                rgt = "R: " + this.Right.Value.ToString();
+
+            return $"{value} {lft} {rgt}";
+        }
+
+
+    }
+}
---
+++ Rukia/Data/IntBinaryNode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nameless.Libraries.Rukia.ProjectEuler.Data
+{
+    public class IntBinaryNode : BinaryNode<int>
+    {
+
+        public int Total
+        {
+            get
+            {
+                if (this.IsRoot)
+                    return this.Value;
+                else if (this.Parent != null && this.Parent.Length == 1)
+                {
+                    return (this.Parent[0] as IntBinaryNode).Total + this.Value;
+                }
+                else if (this.Parent != null && this.Parent.Length == 2)
+                {
+                    if ((this.Parent[0] as IntBinaryNode).Total > (this.Parent[1] as IntBinaryNode).Total)
+                        return this.Value + (this.Parent[0] as IntBinaryNode).Total;
+                    else
+                        return this.Value + (this.Parent[1] as IntBinaryNode).Total;
+                }
+                else
+                    return 0;
+            }
+        }
+
+        public IntBinaryNode(int value)
+            : base(value)
+        {
+        }
+
+
+    }
+}
